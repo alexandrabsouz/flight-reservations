2a4a4467bbb71c4d11b932296873ba7144f0c82e
alexandra <alexandrabsouz@gmail.com>
2022-02-21 14:01:12 -0300

    feat: facilitating order creation

--- lib/bookings/booking.ex
+++ lib/bookings/booking.ex
@@ -13,14 +13,4 @@ defmodule Flightex.Bookings.Booking do
       id: UUID.uuid4()
     }}
   end
-
-  # defp validate_naive_format(complete_date) do
-  #   complete_date
-  #   |> NaiveDateTime.from_iso8601()
-  #   |> handle_date()
-  # end
-
-  # defp handle_date({:ok, complete_date}), do: {:ok, complete_date}
-
-  # defp handle_date({:error, reason}), do: {:error, "date in inválid format"}
 end
